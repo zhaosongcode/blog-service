11272867e85d0c13be2108aafe56d4985fcdaba6
zs <1799240625@qq.com>
2019-10-30 17:13:12 +0800

    整理

---
+++ src/main/java/org/personal/blog/config/common/MyMapper.java
@@ -0,0 +1,11 @@
+package org.personal.blog.config.common;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 通用mapper
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+
+}
